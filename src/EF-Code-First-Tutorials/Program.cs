1b202ff2a2ca9056cc535b2e1abaa3455478e57c
Ron B <ronbnj@gmail.com>
2017-07-22 20:17:57 -0400

    Initial commit

---
+++ src/EF-Code-First-Tutorials/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Code_First_Tutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add and save new Student to DB via EF
+            using (var context = new SchoolContext())
+            {
+                Student student = new Student()
+                {
+                    StudentName = "Ron Buchanan"
+                };
+
+                context.Students.Add(student);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ src/EF-Code-First-Tutorials/SchoolContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Code_First_Tutorials
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Standard> Standards { get; set; }
+    }
+}
